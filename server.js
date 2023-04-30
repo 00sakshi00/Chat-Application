89e62ac954656bae243db5e8e81f6d6242e37150
Sakshi Shukla <92930551+00sakshi00@users.noreply.github.com>
2023-04-30 17:41:32 +0530

    .

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const http = require('http'); 
-
 const app = express();
+const path = require('path');
+const http = require('http').createServer(app); 
+const io= require('socket.io')(http);
 
 const PORT = 3000 || process.env.PORT;
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/index.html');
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg);
+    });
+});
 
 app.listen(PORT,()=>{
     console.log(`server running on port : ${PORT}`);
